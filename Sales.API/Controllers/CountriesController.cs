23473bad37c5555d6741b238666beaf89653e689
erickaurazo <erickaurazo@gmail.com>
2023-02-22 17:02:34 -0500

    Begin countries

---
+++ Sales.API/Controllers/CountriesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sales.API.Data;
+using Sales.Shared.Entities;
+
+namespace Sales.API.Controllers
+{
+    [ApiController]
+    [Route("/api/countries")] // la ruta
+    public class CountriesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CountriesController(DataContext context)
+        {
+            _context = context;
+
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Country country)
+        {
+            //_context.Add(country);
+            _context.Countries.Add(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _context.Countries.ToListAsync());
+        } 
+    }
+
+
+}
