1c4f248a239e4a0db54a4769b4038c19a76099b6
erickaurazo <erickaurazo@gmail.com>
2023-02-22 12:16:12 -0500

    Creación de base de datos

---
+++ Sales.Shared/Entities/Country.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Shared.Entities
+{
+    public class Country
+    {
+
+        public int Id { get; set; }
+
+        [Display(Name = "Pais")]
+        [Required(ErrorMessage = "el campo {0} es obligatorio")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener mas de {1} caractéres")]
+        public string Name { get; set; } = null!;
+
+
+
+
+    }
+}
